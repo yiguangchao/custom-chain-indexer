4cdaae6bdec829fe50b2f9810ee00030e82766f1
GuangchaoYi <1241354012@qq.com>
2025-12-19 23:44:31 +0800

    feat: #4 Persist the data obtained from the chain to the database

--- main.go
+++ main.go
@@ -7,36 +7,62 @@ import (
 	"math/big"
 	"strings"
 
+	"os"
+
 	"custom-chiain-indexer/token"
 
 	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
 )
+
+// 1. Define database model (Entity)
+// Corresponding to the transfer_Logs table in the database
+type TransferLog struct {
+	ID          uint   `gorm:"primaryKey"`
+	TxHash      string `gorm:"type:char(66);uniqueIndex:idx_tx_log"`
+	BlockNumber uint64
+	LogIndex    uint   `gorm:"uniqueIndex:idx_tx_log"`
+	FromAddress string `gorm:"type:char(42);index"`
+	ToAddress   string `gorm:"type:char(42);index"`
+	Amount      string `gorm:"type:varchar(78)"`
+}
 
 func main() {
 	// ---------------------------------------------------------
 	// 1. Connect nodes (Connections)
 	// ---------------------------------------------------------
+	dsn := os.Getenv("DB_DSN")
+	if dsn == "" {
+		dsn = "host=localhost user=postgres password=password dbname=web3_indexer port=5432 sslmode=disable"
+	}
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Database connection failed:", err)
+	}
+	// Auto Migration, similar to Hibernate's ddl auto
+	db.AutoMigrate(&TransferLog{})
+
+	// ---------------------------------------------------------
+	// B. Connect blockchain nodes
+	// ---------------------------------------------------------
 	rpcUrl := "https://mainnet.infura.io/v3/96ca8d4da9ef40c29975cad96332357b"
 	client, err := ethclient.Dial(rpcUrl)
 	if err != nil {
-		log.Fatalf("Unable to connect to node: %v", err)
-	}
-	fmt.Println(">>> Successfully connected to the Ethereum network")
-
-	// ---------------------------------------------------------
-	// 2. Set filtering criteria (Filter)
-	// ---------------------------------------------------------
-	// USDT contract address
-	contractAddress := common.HexToAddress("0xdAC17F958D2ee523a2206206994597C13D831ec7")
-
-	// Get the latest block number and look up 10 blocks ahead
-	header, err := client.HeaderByNumber(context.Background(), nil)
-	if err != nil {
 		log.Fatal(err)
 	}
+
+	// USDT contract
+	contractAddress := common.HexToAddress("0xdAC17F958D2ee523a2206206994597C13D831ec7")
+
+	// Check the last 10 blocks
+	header, _ := client.HeaderByNumber(context.Background(), nil)
 	blockNumber := header.Number.Int64()
 	fromBlock := big.NewInt(blockNumber - 10)
 
@@ -46,48 +72,65 @@ func main() {
 		Addresses: []common.Address{contractAddress},
 	}
 
-	fmt.Printf(">>> Start scanning blocks: %d to %d ...\n", fromBlock, blockNumber)
 	logs, err := client.FilterLogs(context.Background(), query)
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Printf(">>> Scanning completed, found %d logs\n", len(logs))
 
-	contractAbi, err := abi.JSON(strings.NewReader(token.Erc20ABI))
-	if err != nil {
-		log.Fatal("Invalid ABI:", err)
-	}
+	// ---------------------------------------------------------
+	// 3. Core: parsing data (Parsing)
+	// ---------------------------------------------------------
+	contractAbi, _ := abi.JSON(strings.NewReader(token.Erc20ABI))
 
-	type TransferEvent struct {
+	// Temporary structures are used to parse the Data section
+	type TransferEventData struct {
 		Value *big.Int
 	}
 
-	fmt.Println("\n>>> Start parsing detailed data:")
-	fmt.Println("--------------------------------------------------")
+	var dataList []TransferLog
 
 	for _, vLog := range logs {
+		// Verify Event ID (Transfer)
 		if vLog.Topics[0].Hex() != contractAbi.Events["Transfer"].ID.Hex() {
 			continue
 		}
 
-		var event TransferEvent
-		err := contractAbi.UnpackIntoInterface(&event, "Transfer", vLog.Data)
+		// 1. Analyze Data (Amount)
+		var eventData TransferEventData
+		err := contractAbi.UnpackIntoInterface(&eventData, "Transfer", vLog.Data)
 		if err != nil {
-			log.Printf("Failed to parse data: %v", err)
+			log.Printf("Parsing failed: %v", err)
 			continue
 		}
 
-		from := common.HexToAddress(vLog.Topics[1].Hex())
-		to := common.HexToAddress(vLog.Topics[2].Hex())
+		// 2. Analyzing Topics (From, To)
+		from := common.HexToAddress(vLog.Topics[1].Hex()).Hex()
+		to := common.HexToAddress(vLog.Topics[2].Hex()).Hex()
 
-		fmt.Printf(
-			"Blocks: %d | TX: %s\n   from: %s\n   to: %s\n   amount: %s (unit)\n",
-			vLog.BlockNumber,
-			vLog.TxHash.Hex(),
-			from.Hex(),
-			to.Hex(),
-			event.Value.String(),
-		)
-		fmt.Println("--------------------------------------------------")
+		// 3. Build Model Object
+		logModel := TransferLog{
+			TxHash:      vLog.TxHash.Hex(),
+			BlockNumber: vLog.BlockNumber,
+			LogIndex:    vLog.Index,
+			FromAddress: from,
+			ToAddress:   to,
+			Amount:      eventData.Value.String(),
+		}
+
+		dataList = append(dataList, logModel)
+	}
+
+	// 4. Batch Insert
+	if len(dataList) > 0 {
+		// Clause (clause. OnConflict...) Similar to SQL's INSERTIGNORE or ON DUPLICATE KEY UPDATE
+		// Prevent duplicate processing of the same block from causing database errors
+		result := db.Clauses(clause.OnConflict{DoNothing: true}).Create(&dataList)
+
+		if result.Error != nil {
+			log.Printf("Failed to put in storage: %v", result.Error)
+		} else {
+			fmt.Printf(">>> Successfully stored in the warehouse %d pieces of data！\n", result.RowsAffected)
+		}
 	}
 }
